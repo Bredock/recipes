06cd9cb717666dbdd217b82da6dc18bbf07c9139
luis <luisbarbafuster@gmail.com>
2018-10-03 20:18:39 +0200

    Angular Course Project Basics

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test',
+      'https://images-gmi-pmc.edge-generalmills.com/2157c0dc-2a0e-4039-9339-1eccfd4c9556.jpg'),
+    new Recipe('A test recipe', 'This is simply a test',
+      'https://images-gmi-pmc.edge-generalmills.com/2157c0dc-2a0e-4039-9339-1eccfd4c9556.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
